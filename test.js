dd8d90dca48df21f2562167d7d7eb444c109cca9
tom.yu <tom.yu@helimediagroup.com>
2015-04-08 15:43:03 +0800

    remove personal info

--- test.js
+++ test.js
@@ -1,8 +1,3 @@
 var ks = require('keepstreak');
-var config = {
-	username: 'dayuoba',
-	password: 'de1depiao',
-	remoteaddr: 'github.com/dayuoba/streak',
-	trackFile: 'tracking/tracking.md'
-}
+var config = require('./config.json');
 ks(config);
