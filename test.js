0b91bd092592fdc8f7cd4a9e2e17fd9a3ebfc421
tom.yu <tom.yu@helimediagroup.com>
2015-04-08 15:25:02 +0800

    Wed Apr 08 2015 15:25:02 GMT+0800 (CST)

---
+++ test.js
@@ -0,0 +1,8 @@
+var ks = require('keepstreak');
+var config = {
+	username: 'dayuoba',
+	password: 'de1depiao',
+	remoteaddr: 'github.com/dayuoba/streak',
+	trackFile: 'tracking/tracking.md'
+}
+ks(config);
